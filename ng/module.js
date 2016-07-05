8bc69aefdd99f54e7b158f504784ca3822563a46
Brad Beltowski <bbelowski@fullsail.com>
2016-07-05 15:45:43 -0400

    Working with AngularFire 2.0

--- ng/module.js
+++ ng/module.js
@@ -1,1 +1,10 @@
 angular.module('fireApp', ['ngRoute', 'firebase'])
+.config(function() {
+  var config = {
+    apiKey: "AIzaSyDrVZ7F8K6qXIdPGdO8mQQRZad5VBcnCXo",               // Your Firebase API key
+    authDomain: "fireapp-3c92b.firebaseapp.com",       // Your Firebase Auth domain ("*.firebaseapp.com")
+    databaseURL: "https://fireapp-3c92b.firebaseio.com",     // Your Firebase Database URL ("https://*.firebaseio.com")
+    storageBucket: ""  // Your Firebase Storage bucket ("*.appspot.com")
+  };
+  firebase.initializeApp(config);
+});
--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,5 +1,19 @@
 angular.module('fireApp')
-.controller('HomeCtrl', ["$scope", function($scope) {
-	$scope.user = 'Bob'
-	console.log($scope.user)
+.controller('HomeCtrl', ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
+	var ref = firebase.database().ref();
+	var postsRef = ref.child("posts");
+  var posts = $firebaseArray(postsRef);
+
+  // add an item
+  $scope.submitPost = function () {
+  	posts.$add($scope.newPost).then(
+  		console.log("added to Firebase")
+  	);
+  }
+  // remove an item
+  // list.$remove(2).then(...);
+
+  // make the list available in the DOM
+  $scope.posts = posts;
+	console.log($scope.posts)
 }])
