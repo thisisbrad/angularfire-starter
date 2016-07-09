f1f4f658953e8a06c2d6ef28daa73d142dbdd580
Brad Beltowski <bbelowski@fullsail.com>
2016-07-09 13:16:08 -0400

    Finished Simple Angularfire CRUD

--- ng/module.js
+++ ng/module.js
@@ -1,1 +1,10 @@
 angular.module('fireApp', ['ngRoute', 'firebase'])
+.config(function() {
+	var config = {
+    apiKey: "AIzaSyDrVZ7F8K6qXIdPGdO8mQQRZad5VBcnCXo",
+    authDomain: "fireapp-3c92b.firebaseapp.com",
+    databaseURL: "https://fireapp-3c92b.firebaseio.com",
+    storageBucket: "fireapp-3c92b.appspot.com",
+  };
+  firebase.initializeApp(config);
+})
